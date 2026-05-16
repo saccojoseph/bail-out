632079d20345dfde50310428e3aedbf58b96c72d
Joseph Sacco <saccojoseph@gmail.com>
2026-05-15 23:17:31 -0400

    Initial Commit

---
+++ Bail/BailApp.swift
@@ -0,0 +1,17 @@
+//
+//  BailApp.swift
+//  Bail
+//
+//  Created by Joseph Sacco on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct BailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bail/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bail
+//
+//  Created by Joseph Sacco on 5/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
